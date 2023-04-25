c31213c6be0d803b29f8a08dfda86494e5a96ef2
Sindre Stephansen <sindre@sindrestephansen.com>
2023-04-25 15:49:15 +0200

    Take cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 mod parser;
 
-use std::fs;
+use std::{env, fs};
+use std::io::{Read, stdin};
+use std::process::exit;
 use crate::parser::parse;
 
+fn print_usage(prog: &str) {
+    println!("USAGE: {prog} FILE")
+}
+
 fn main() {
-    let file = fs::read_to_string("tests/test.kw").expect("cannot read file");
-    parse(&file);
+    let args: Vec<String> = env::args().collect();
+
+    let input = match &args.get(1) {
+        Some(path) => {
+            fs::read_to_string(path).expect("Cannot read file")
+        }
+        None => {
+            print_usage(&args[0]);
+            println!("No file provided");
+            exit(-1);
+        }
+    };
+
+    parse(&input);
 }
