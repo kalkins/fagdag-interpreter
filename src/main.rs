17c1186dc7f1143a73593fe69deb5ae474907ced
Sindre Stephansen <sindre@sindrestephansen.com>
2023-04-25 15:26:08 +0200

    Define the language grammar, and parse it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate pest;
+extern crate pest_derive;
+
+use std::fs;
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "kwlang.pest"]
+struct KWLangParser;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("tests/test.kw").expect("cannot read file");
+
+    match KWLangParser::parse(Rule::program, &file) {
+        Ok(program) => {
+            println!("{}", program.to_json());
+        }
+        Err(error) => {
+            print!("Error during parsing:\n{}", error);
+        }
+    }
 }
